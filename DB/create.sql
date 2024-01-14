713a71b91a15c93af13cf374100e07f5179c5d7d
schoolstudent16 <146916958+schoolstudent16@users.noreply.github.com>
2024-01-14 17:58:43 +0900

    CREATE文作成

---
+++ DB/create.sql
@@ -0,0 +1,23 @@
+Create Table account(
+    user_id INTEGER,
+    user_name VARCHAR(80) NOT NULL,
+    mail_address VARCHAR(80) NOT NULL,
+    password VARCHAR(50) NOT NULL,
+    PRIMARY KEY (user_id)
+);
+Create Table category(
+    category_id INTEGER,
+    category_name VARCHAR(80) NOT NULL,
+    PRIMARY KEY (category_id)
+);
+Create Table words(
+    word_id INTEGER,
+    user_id INTEGER,
+    category_id INTEGER NOT NULL,
+    register_date DATE NOT NULL,
+    word VARCHAR(100) NOT NULL,
+    word_content VARCHAR(300) NOT NULL,
+    PRIMARY KEY (word_id,user_id),
+    FOREIGN KEY (user_id) REFERENCES user(user_id)
+);
+
